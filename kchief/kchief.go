f82d5a69bff227d59d6d8fbdbc652485b0fe6764
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-09-11 12:12:07 +0200

    refactored and added comments

--- kchief/kchief.go
+++ kchief/kchief.go
@@ -24,48 +24,64 @@ import (
 //							|- Timestamp
 //							|- Value
 
+// readPBMessag will read the protobuf binary file from disk,
+// unmarshall the Message struct from the proto spesification,
+// unzip the
 func readPBMessage() error {
 	b, err := ioutil.ReadFile("./sample/sample2.bin")
 	if err != nil {
 		return fmt.Errorf("error: readData, os.Open failed %v", err)
 	}
 
-	// The main protobuf message seems to be in .Message
+	// unmarshall the Message struct from the protobuf raw data
 	message := messagingpb.Message{}
 	err = proto.Unmarshal(b, &message)
 	if err != nil {
 		log.Printf("error: failed Unmarshal Message: %v\n", err)
 	}
 
-	// Message for a field called Data which seems to be compress with gzip.
-	messageData := message.GetData()
-	dataReader := bytes.NewReader(messageData)
+	// Get the compressed content of the field Data,
+	// and uncompress it.
+	dataUncompressed := getDataFieldFromMessage(message)
 
-	gzipReader, err := gzip.NewReader(dataReader)
-	if err != nil {
-		log.Printf("error: gzip newreader failed: %v\n", err)
-	}
-
-	dataPayloads, err := ioutil.ReadAll(gzipReader)
-	if err != nil {
-		log.Printf("error: readall gzipReader failed: %v\n", err)
-	}
-
-	// fmt.Println(dataPayloads)
-
+	// unmarshal Payloads from Data.
 	payloads := messagingpb.Payloads{}
-
-	err = proto.Unmarshal(dataPayloads, &payloads)
+	err = proto.Unmarshal(dataUncompressed, &payloads)
 	if err != nil {
 		log.Printf("error: failed Unmarshal Message: %v\n", err)
 	}
 
-	getPayloads(payloads)
+	getPayload(payloads)
 
 	return nil
 }
 
-func getPayloads(payloads messagingpb.Payloads) {
+// getDataField uncompresses the compress field Data in
+// message, and returns the result.
+func getDataFieldFromMessage(message messagingpb.Message) []byte {
+
+	// The Data field in the Message struct contains
+	// compress data that we need to decompress.
+	messageDataCompressed := message.GetData()
+	dataCompressedReader := bytes.NewReader(messageDataCompressed)
+
+	gzipReader, err := gzip.NewReader(dataCompressedReader)
+	if err != nil {
+		log.Printf("error: gzip newreader failed: %v\n", err)
+	}
+
+	dataUncompressed, err := ioutil.ReadAll(gzipReader)
+	if err != nil {
+		log.Printf("error: readall gzipReader failed: %v\n", err)
+	}
+
+	return dataUncompressed
+}
+
+// getPayload iterates over all the Payloads,
+// then for each respective payload it finds
+// the TAG name, and the corresponding TagdataPoint.
+func getPayload(payloads messagingpb.Payloads) {
 	for _, v := range payloads.Payloads {
 		// fmt.Printf("index %v : %#v\n", i, v.GetTagdatapoints())
 		vv := v.GetTagdatapoints()
