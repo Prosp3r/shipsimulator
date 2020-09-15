c6226de1695385eb55eaa50f0261a0270e015747
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-09-15 13:33:42 +0200

    publisher working

--- kchief/cmd/mqttpublish/main.go
+++ kchief/cmd/mqttpublish/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -61,12 +63,39 @@ func unSubscribe(c mqtt.Client, topic string) error {
 	return nil
 }
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 func main() {
+	topic := flag.String("topic", "CloudBoundContainer", "The name of the MQTT topic")
+	broker := flag.String("broker", "10.0.0.26", "The ip address of the MQTT broker")
+	port := flag.String("port", "1883", "The port where the MQTT broker listens")
+	protocol := flag.String("protocol", "tcp", "The protocol to use when connecting to the MQTT broker")
+	clientID := flag.String("clientID", "btclient", "The client ID to use with MQTT")
+	var inFile arrayFlags
+	flag.Var(&inFile, "inFile", "specify the files to use as input comma separated")
+	repetitions := flag.Int("repetitions", 1, "specify how many repetitions to run")
+	delay := flag.Int("delay", 300, "The number of milliseconds to wait between each mqtt publish")
+
+	flag.Parse()
+
+	if len(inFile) == 0 {
+		fmt.Printf("no input files, use the --protoFiles flag to specify one or mores files to use as input.\n")
+		return
+	}
+
 	var err error
-	topic := "CloudBoundContainer"
 
 	// Create new mqtt client
-	client, err := NewMQTTClient("tcp", "10.0.0.26", "1883", "btclient")
+	client, err := NewMQTTClient(*protocol, *broker, *port, *clientID)
 	if err != nil {
 		log.Printf("error: newMQTTClient failed: %v\n", err)
 		return
@@ -75,7 +104,7 @@ func main() {
 
 	// Subscribe to topic,
 	// subscribe will also print the result to console.
-	err = subscribe(client, topic)
+	err = subscribe(client, *topic)
 	if err != nil {
 		log.Printf("error: mqtt client subscribe failed: %v\n", err)
 		return
@@ -84,15 +113,31 @@ func main() {
 	msgCh := make(chan interface{})
 
 	//start publish'er
-	go publish(client, topic, msgCh)
+	go publish(client, *topic, msgCh)
 
 	// send some data to the publisher channel
-	for i := 0; i < 10; i++ {
-		msgCh <- i
-		time.Sleep(time.Millisecond * 300)
+	for i := 0; i < *repetitions; i++ {
+		fmt.Printf("protoFiles contains : %#v\n", inFile)
+		for _, v := range inFile {
+			fmt.Printf("v contains : %v\n", v)
+			fh, err := os.Open(v)
+			if err != nil {
+				log.Printf("error: failed to open protoFile: %v\n", err)
+				return
+			}
+
+			b, err := ioutil.ReadAll(fh)
+			if err != nil {
+				log.Printf("error: file ReadAll failed: %v\n", err)
+			}
+			fh.Close()
+
+			msgCh <- b
+			time.Sleep(time.Millisecond * time.Duration(*delay))
+		}
 	}
 
-	err = unSubscribe(client, topic)
+	err = unSubscribe(client, *topic)
 	if err != nil {
 		log.Printf("error: mqtt client unSubscribe failed: %v\n", err)
 		return
