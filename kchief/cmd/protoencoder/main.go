ebfd86b23e982d27c7baea99f8a06736a35053f9
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-09-16 10:31:39 +0200

    updated comments

--- kchief/cmd/protoencoder/main.go
+++ kchief/cmd/protoencoder/main.go
@@ -7,10 +7,6 @@ import (
 	"github.com/RaaLabs/shipsimulator/kchief"
 )
 
-// TODO:
-// - Specify output file
-// - Read tags to create for from JSON file to replace the hard coded values in the protogenerator.go file
-
 func main() {
 	outBinFile := flag.String("outFile", "myfile.bin", "specify the filename of the output file")
 	inJsonFile := flag.String("inJsonFile", "", "specify the full path of the json file to take as input")
