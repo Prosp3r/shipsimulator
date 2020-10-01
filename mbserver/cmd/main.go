ce11b93080ae42ede4c82f9577a35c98eea8f533
Bjørn Tore Svinningen <postmannen@gmail.com>
2020-10-01 07:58:43 +0200

    started adding multiple config files handling

--- mbserver/cmd/main.go
+++ mbserver/cmd/main.go
@@ -6,6 +6,28 @@
 	- byte level within a word.
 	- word level where each of the uint16's have swapped place.
 
+	Function codes:
+	1, read coils
+	2, read descrete inputs
+	3, read holding registers
+	4, read input registers
+
+	Snip from https://www.csimn.com/CSI_pages/Modbus101.html
+	--------------------------------------------------------
+	Valid address ranges as originally defined for Modbus were 0 to 9999 for each of the above register types. Valid ranges allowed in the current specification are 0 to 65,535. The address range originally supported by Babel Buster gateways was 0 to 9999. The extended range addressing was later added to all new Babel Buster products.
+	The address range applies to each type of register, and one needs to look at the function code in the Modbus message packet to determine what register type is being referenced. The Modicon convention uses the first digit of a register reference to identify the register type.
+	Register types and reference ranges recognized with Modicon notation are as follows:
+	0x = Coil = 00001-09999
+	1x = Discrete Input = 10001-19999
+	3x = Input Register = 30001-39999
+	4x = Holding Register = 40001-49999
+	On occasion, it is necessary to access more than 10,000 of a register type. Based on the original convention, there is another de facto standard that looks very similar. Additional register types and reference ranges recognized with Modicon notation are as follows:
+	0x = Coil = 000001-065535
+	1x = Discrete Input = 100001-165535
+	3x = Input Register = 300001-365535
+	4x = Holding Register = 400001-465535
+	When using the extended register referencing, it is mandatory that all register references be exactly six digits. This is the only way Babel Buster will know the difference between holding register 40001 and coil 40001. If coil 40001 is the target, it must appear as 040001.
+
 	References :
 	https://control.com/forums/threads/confused-modbus-tcp-vs-modbus-rtu-over-tcp.37377/
 	https://www.simplymodbus.ca/TCP.htm
@@ -22,9 +44,15 @@ import (
 	"math"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/RaaLabs/shipsimulator/mbserver"
 )
+
+type functionRegisterFiles struct {
+	name string
+	fh   *os.File
+}
 
 func main() {
 	// Start a new server
@@ -39,34 +67,33 @@ func main() {
 	// registry data like postions in slice etc.
 	const startReg int = 100 // TODO: Replace this with parsed start value from input
 
-	// // Create some input data to fill the register
-	// registryData := []encoder{
-	// 	float32LittleWordBigEndian{
-	// 		Type:   "float32LittleWordBigEndian",
-	// 		Number: 3.1415,
-	// 		Size:   2,
-	// 	},
-	// 	float32BigWordBigEndian{
-	// 		Type:   "float32BigWordBigEndian",
-	// 		Number: 3.141516,
-	// 		Size:   2,
-	// 	},
-	// }
+	// The configuration is split in 4 files, 1 for each register
+	fileNames := []string{"coil.json", "descrete.json", "input.json", "holding.json"}
+	var fhs []functionRegisterFiles
 
-	js := []byte(`[{
-		"type":"float32LittleWordBigEndian",
-		"number":3.1415,
-		"size":2
-	}, {
-		"type":"float32BigWordBigEndian",
-		"number":3.141516,
-		"size":2
-		}]`)
+	// Iterate over all the filenames specified, and create a holding
+	// strcuture to keep all the file handles in with info about each
+	// register.
+	for _, v := range fileNames {
+		fh, err := os.Open(v)
+		if err != nil {
+			log.Printf("error: failed to open config file for %v: %v\n", v, err)
+		}
 
+		fName := strings.Split(v, ".")
+		f := functionRegisterFiles{
+			name: fName[0],
+			fh:   fh,
+		}
+		fhs = append(fhs, f)
+		defer fh.Close()
+	}
 	objs := []map[string]interface{}{}
-	err = json.Unmarshal(js, &objs)
+	js := json.NewDecoder(fhs[3].fh)
+	err = js.Decode(&objs)
+	//err = json.Unmarshal(js, &objs)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("error: decoding json: %v\n", err)
 	}
 
 	var registryData []encoder
