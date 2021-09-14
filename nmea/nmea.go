d910f845a8a5d626042634c58d6804081f965186
postmannen <postmannen@gmail.com>
2021-09-14 11:48:30 +0200

    added context cancelation to all go routines

--- nmea/nmea.go
+++ nmea/nmea.go
@@ -71,6 +71,8 @@ func (s *server) Run() {
 		}
 	}()
 
+	// Start the connections handler that will do the broadcast of
+	// the nmea data to all active network connections.
 	wg.Add(1)
 	go func() {
 		err := s.connections.handle(ctx)
@@ -80,8 +82,17 @@ func (s *server) Run() {
 		wg.Done()
 	}()
 
-	go s.readFile()
+	// Start the reading from file.
+	wg.Add(1)
+	go func() {
+		err := s.readFile(ctx)
+		if err != nil {
+			log.Printf("%v\n", err)
+		}
+		wg.Done()
+	}()
 
+	// Check for ctrl+c signal to initiate shutdown.
 	go func() {
 		sigCh := make(chan os.Signal, 1)
 		signal.Notify(sigCh, os.Interrupt)
@@ -90,26 +101,37 @@ func (s *server) Run() {
 		sig := <-sigCh
 		fmt.Printf("Got exit signal, terminating all processes, %v\n", sig)
 
+		// Adding a safety function here so we can make sure that all processes
+		// are stopped after a given time if the context cancelation hangs.
+		go func() {
+			time.Sleep(time.Second * 3)
+			log.Printf("error: doing a non graceful shutdown of all processes..\n")
+			os.Exit(1)
+		}()
+
 		cancel()
-
-		wg.Wait()
-
-		os.Exit(0)
 	}()
 
+	// Pick up the nmea data read from file, and put it on the newData
+	// channel to be broadcasted to all connections.
 	for v := range s.nmeaReadCh {
 		s.connections.newData <- []byte(v)
 	}
-	// }
 
+	wg.Wait()
 }
 
+// connections hold all the information about new connections,
+// new data to be delivered to the connections registered,
+// and a register conns of all the registered connections.
 type connections struct {
 	newConn chan net.Conn
 	newData chan []byte
 	conns   map[net.Conn]bool
 }
 
+// newConnections will return a connections struct with all
+// fields prepared for use.
 func newConnections() *connections {
 	c := connections{
 		newConn: make(chan net.Conn),
@@ -119,6 +141,9 @@ func newConnections() *connections {
 	return &c
 }
 
+// handle will add and remove connections to the conns connection
+// register map, and also take care of broadcasting the new data
+// received out to all the active connections.
 func (c *connections) handle(ctx context.Context) error {
 	for {
 		select {
@@ -177,7 +202,10 @@ func (c *connections) handle(ctx context.Context) error {
 	}
 }
 
-func (s *server) readFile() error {
+// readFile will continously read the file with the nmea data, and deliver
+// them out on a timed interval to the nmeaReadCh to be picked up by the
+// connections.handler.
+func (s *server) readFile(ctx context.Context) error {
 	// Open the nmea file for reading
 	f, err := os.Open(s.nmeaFile)
 	if err != nil {
@@ -188,22 +216,27 @@ func (s *server) readFile() error {
 	scanner := bufio.NewScanner(f)
 	ticker := time.NewTicker(time.Duration(s.delay) * time.Microsecond)
 
-	for range ticker.C {
-		// Check if there are more to scan
-		for scanner.Scan() {
-			sentence, err := nmea.Parse(scanner.Text())
-			if err != nil {
-				return fmt.Errorf("error: failed to parse nmea sentence: %v", err)
-			}
+	for {
+		select {
+		case <-ticker.C:
+			// Check if there are more to scan
+			for scanner.Scan() {
+				sentence, err := nmea.Parse(scanner.Text())
+				if err != nil {
+					return fmt.Errorf("error: failed to parse nmea sentence: %v", err)
+				}
 
-			if sentence.DataType() == nmea.TypeRMC {
-				rmc := sentence.(nmea.RMC).String()
-				s.nmeaReadCh <- rmc
-				break
-			}
+				if sentence.DataType() == nmea.TypeRMC {
+					rmc := sentence.(nmea.RMC).String()
+					s.nmeaReadCh <- rmc
+					fmt.Printf("* rmc: %v\n", rmc)
+					break
+				}
 
+			}
+		case <-ctx.Done():
+			close(s.nmeaReadCh)
+			return fmt.Errorf("info: readFile: got done signal")
 		}
-
 	}
-	return nil
 }
